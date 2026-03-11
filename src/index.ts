e1d8a2a2800d3059222b3e457173b18e19493f2b
Vouchyk Vova <nechayvova2005@gmail.com>
2026-03-11 19:00:28 +0000

    Change cors rules and env variable

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: [process.env.CLIENT_URL || "", "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
